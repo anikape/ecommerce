f5c2e73c5d9b6e0774ad35f34c81b1fc7b0c839d
DJehSantana <jessicah.dias23@hotmail.com>
2022-08-16 20:33:31 -0300

    verificação de inputs vazios

--- JS/cadastro.js
+++ JS/cadastro.js
@@ -1,42 +1,44 @@
+//criando formData para receber valores dos inputs
+const formData = new FormData(document.querySelector('form'));
+
 //importando elementos
 const form = document.getElementById('form');
-const nome = document.getElementById('nome');
-const email = document.getElementById('email');
-const cep = document.getElementById('cep');
-const endereco = document.getElementById('endereco');
-const numero = document.getElementById('numero');
-const bairro = document.getElementById('bairro');
-const cidade = document.getElementById('cidade');
-const estado = document.getElementById('estado');
-const senha = document.getElementById('senha');
-const confirmarSenha = document.getElementById('confirmarSenha');
-
+const nome = formData.get('nome');
+const email = formData.get('email');
+const cep = formData.get('cep');
+const endereco = formData.get('endereco');
+const numero = formData.get('numero');
+const bairro = formData.get('bairro');
+const cidade = formData.get('cidade');
+const estado = formData.get('estado');
+const senha = formData.get('senha');
+const confirmarSenha = formData.get('confirmarSenha');
+const errorMessage = document.getElementsByClassName('error-message');
+const formControl = document.getElementsByClassName('input-group');
 
 //checagem dos inputs vazios
-
-form.addEventListener('submit', checkInputs());
-
 function checkInputs(){
 
     const formulario = [
-        nomeValido = nome.value.trim(),
-        emailValido = email.value.trim(),
-        cepValido = cep.value.trim(),
-        enderecoValido = endereco.value.trim(),     
-        numeroValido = numero.value.trim(),
-        bairroValido = bairro.value.trim(),
-        cidadeValida = cidade.value.trim(),
-        estadoValido = estado.value.trim(),
-        senhaValida  = senha.value.trim(),
-        senhaConfirmada = confirmarSenha.value.trim()
+        nome, 
+        email,
+        cep,
+        endereco,
+        numero, 
+        bairro,
+        cidade,
+        estado,
+        senha,
+        confirmarSenha
     ]
 
+    
+
     for(let campo of formulario){
-        if(!campo) {
-            //caso o campo esteja vazio, mostrar mensagem de erro
-            let formControl = document.getElementsByClassName('input-group');
-            const small = formControl.querySelector('small');
-            small.innerText = "Campo obrigatório!";
+        if(!campo.trim()) {
+            //caso o campo esteja vazio, mostrar mensagem de erro  
+            const message = "Campo obrigatório!"         
+            errorMessage.inerText = message;
             //atribuir classe error
             formControl.classList.add('error');            
         }else{
@@ -45,4 +47,14 @@ function checkInputs(){
     }
 
     //verificar se senhas são iguais
+    if (senha !== confirmarSenha){
+        const message = "Senhas devem ser iguais!"
+        errorMessage.inerText = message;
+        formControl.classList.add('error');
+    } else{
+        formControl.classList.add('success');
+    }
 }
+
+
+form.addEventListener('submit', checkInputs());
