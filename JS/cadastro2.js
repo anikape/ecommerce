fb0550293693b4061deef0b70fc6523168b57ed9
DJehSantana <jessicah.dias23@hotmail.com>
2022-08-17 16:49:04 -0300

    atualização e refatoração de filtros js

---
+++ JS/cadastro2.js
@@ -0,0 +1,62 @@
+//criando formData para receber valores dos inputs
+const formData = new FormData(document.querySelector('form'));
+
+//importando elementos
+const form = document.getElementById('form');
+const nome = formData.get('nome');
+const email = formData.get('email');
+const cep = formData.get('cep');
+const endereco = formData.get('endereco');
+const numero = formData.get('numero');
+const bairro = formData.get('bairro');
+const cidade = formData.get('cidade');
+const estado = formData.get('estado');
+const senha = formData.get('senha');
+const confirmarSenha = formData.get('confirmarSenha');
+const errorMessage = document.getElementsByClassName('error-message');
+const formControl = document.getElementsByClassName('input-group');
+
+//checagem dos inputs vazios
+function checkInputs(){
+
+    const formulario = [
+        nome, 
+        email,
+        cep,
+        endereco,
+        numero, 
+        bairro,
+        cidade,
+        estado,
+        senha,
+        confirmarSenha
+    ]
+
+    
+
+    for(let campo of formulario){
+        if(!campo) {
+            console.log('Campos vazios!')
+            //caso o campo esteja vazio, mostrar mensagem de erro  
+            //const message = "Campo obrigatório!"         
+            //errorMessage.inerText = message;
+            //atribuir classe error
+            //formControl.classList.add('error');            
+        }
+    }
+
+    //verificar se senhas são iguais
+    if (senha === confirmarSenha){
+        console.log('senhas iguais')
+        
+        //const message = "Senhas devem ser iguais!"
+        //errorMessage.inerText = message;
+        //formControl.classList.add('error');
+    } else{
+        console.log('senhas diferentes')
+        //formControl.classList.add('success');
+    }
+}
+
+
+
